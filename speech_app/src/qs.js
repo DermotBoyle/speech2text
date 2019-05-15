57b1c58bed9803eb33b371b09c7c6a4f2e59602a
Brenda <brenda@brenda.com>
2019-05-15 16:21:56 +0200

    new code

--- speech_app/src/qs.js
+++ speech_app/src/qs.js
@@ -1,63 +1,67 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import {Button} from 'reactstrap';
+import axios from 'axios'
+import TweetBtn from './tweet'
+
 
 class Quote extends Component {
-  constructor() {
-    super();
-    this.state = {
-      quote: {
-        content: "",
-        link: "",
-        title: "",
-        data: "data"
-      },
-      hasQuote: false
-    };
+  state = {
+    quote: '',
+    author: '',
+  };
+  
+  componentDidMount() {
+    this.getQuote()
+  }
+  
+  getQuote() {
+    let url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
-    this.endpoint =
-      "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=8";
+    axios.get(url)
+      .then(res => {
+        let randomQuote = Math.floor(Math.random() * res.data.quotes.length)
+        
+        this.setState({
+          quote: res.data.quotes[randomQuote]['quote'],
+          author: res.data.quotes[randomQuote]['author']
+
+        })
+      })
   }
 
-  getRandomQuote = event => {
-    fetch(this.endpoint)
-      .then(response => response.json())
-      .then(data => {
-        if (data[0].content && data[0].title && data[0].link) {
-          let { quote } = this.state;
-          let quoteData = data[0];
-          quote.content = quoteData.content;
-          quote.link = quoteData.link;
-          quote.title = quoteData.title;
-
-          this.setState({ quote }, () => {
-            if (this.state.hasQuote === false) {
-              this.setState({ hasQuote: true });
-            }
-          });
-        } else {
-          return console.error("No Quote");
-        }
-      });
-  };
-
-  renderQuote = () => {
-    const { title, content } = this.state.quote;
-    return (
-      <div>
-        <h1>{title}</h1>
-        <p>{content}</p>
-      </div>
-    );
+  getNewQuote = () => {
+    this.getQuote()
   };
 
   render() {
+
     return (
-      <Fragment>
-        <Button onClick={this.getRandomQuote}>Quotes</Button>
-        {this.state.hasQuote === true ? this.renderQuote() : " "}
-      </Fragment>
+      <div>
+          <Display quote={this.state.quote} author={this.state.author}/>
+          <div id="buttons">
+            <NewQuoteBtn changeQuote={this.getNewQuote} />
+            <TweetBtn quote={this.state.quote} author={this.state.author}/>
+          </div>
+      </div>
     );
   }
 }
+
+const NewQuoteBtn = (props) => {
+    return (
+      <Button onClick={props.changeQuote}>New Quote</Button>
+    );
+}
+
+const Display = (props) => {
+    return (
+        <div className="display">
+            <p>{props.quote}</p>
+            <h5>{props.author}</h5>
+        </div>
+    )
+}
+
+
 
 export default Quote;
---
+++ speech_app/src/tweet.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Button } from 'reactstrap';
+import { FaTwitter} from "react-icons/fa";
+
+export default class TweetBtn extends Component {
+    tweetBtn = () => {
+        console.log("Tweet me")
+      }
+       render() {
+           
+         return <a href={`https://twitter.com/intent/tweet?text= ${this.props.quote} ${this.props.author}`} target=" ">
+             <Button  onClick={this.tweetBtn}>
+               Tweet
+            <FaTwitter/>
+             </Button>
+           </a>;     
+      }
+    }
+    
